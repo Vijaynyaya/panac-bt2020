d8ad0a9ee9b25c5c3dd70031e70b972a2fcaa4b5
Vijaynyaya <78458631+Vijaynyaya@users.noreply.github.com>
2021-12-01 14:33:36 +0530

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-const getH = async _ => {
-    let hist = await fetch('https://disease.sh/v3/covid-19/historical')
-    console.log(hist)
-    hist = await hist.json()
-    console.log(hist, 'JSON')
-    return hist.map(each => each['country'])
-}
-
